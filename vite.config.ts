593a7f7a6f5951a62001cb2f93a12626e41c2e1a
Alex Bashlii <oleksandr@bstock.com>
2023-11-27 04:33:41 +0200

    gh page url fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   const config = {
     plugins: [react()],
-    base: "/weather-app",
+    base: "/open-weather-app",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src")
